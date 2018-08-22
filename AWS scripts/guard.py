237b3b5e216a9ab684ab8d862f4ee81be6a36d59
orda <orda@CentOS7-vm.localdomain>
2018-08-22 12:12:14 +0300

    Change output to csv

--- AWS scripts/guard.py
+++ AWS scripts/guard.py
@@ -2,46 +2,62 @@
 import boto3
 import csv
 
-session = boto3.Session(profile_name='pdf-prod')
-guardClient = session.client('guardduty')
-detector = guardClient.list_detectors()['DetectorIds'][0]
+with open("guardduty.csv", 'w', newline='') as csvfile:
+	header = ['Type', 'InstanceId', 'InstanceName', 'Count', 'Severity']
+	writer = csv.DictWriter(csvfile, fieldnames = header)
+	writer. writeheader()
 
-findingPaginator = guardClient.get_paginator('list_findings')
-findingsPages = findingPaginator.paginate(
-	DetectorId=detector,
-	FindingCriteria={
-		'Criterion':{
-			'severity':{
-				'Gte': 0,
-				'Lt': 4
+	session = boto3.Session(profile_name='pdf-prod')
+	guardClient = session.client('guardduty')
+	detector = guardClient.list_detectors()['DetectorIds'][0]
+
+	findingPaginator = guardClient.get_paginator('list_findings')
+	findingsPages = findingPaginator.paginate(
+		DetectorId=detector,
+		FindingCriteria={
+			'Criterion':{
+				'severity':{
+					'Gt': 0,
+					'Lt': 4
+				}
 			}
 		}
-	}
-)
-for page in findingsPages:
-	#print(page)
-	for currentFindingId in page['FindingIds']:
-		#print("ID:"+currentFinding)
-		info = guardClient.get_findings(
-			DetectorId=detector,
-			FindingIds=[currentFindingId]
-		)
-		findingType =  info['Findings'][0]['Type']
-		print(findingType)
-		findingCount = str(info['Findings'][0]['Service']['Count'])
-		print(findingCount)
-		severity = str(info['Findings'][0]['Severity'])
-		print(severity)
-		instanceId = info['Findings'][0]['Resource']['InstanceDetails']['InstanceId']
-		print(instanceId)
-		try:
-			for tag in info['Findings'][0]['Resource']['InstanceDetails']['Tags']:
-				if tag['Key'] == 'Name':
-					instanceName = tag['Value']
-					print(instanceName)
-					break
-		except Exception:
-			instanceName = "No name"
-			print(instanceName)
-			continue
-		print('================================================================')
+	)
+	counter = 1;
+	for page in findingsPages:
+		#print(page)
+		for currentFindingId in page['FindingIds']:
+			print("Processing finding #"+str(counter))
+			#print("ID:"+currentFinding)
+			info = guardClient.get_findings(
+				DetectorId=detector,
+				FindingIds=[currentFindingId]
+			)
+			findingType =  info['Findings'][0]['Type']
+			#print(findingType)
+			findingCount = str(info['Findings'][0]['Service']['Count'])
+			#print(findingCount)
+			severity = str(info['Findings'][0]['Severity'])
+			#print(severity)
+			instanceId = info['Findings'][0]['Resource']['InstanceDetails']['InstanceId']
+			#print(instanceId)
+			try:
+				for tag in info['Findings'][0]['Resource']['InstanceDetails']['Tags']:
+					if tag['Key'] == 'Name':
+						instanceName = tag['Value']
+						#print(instanceName)
+						break
+			except Exception:
+				instanceName = "No name"
+				#print(instanceName)
+				continue
+			#print('================================================================')
+			row = {
+				'Type': findingType,
+				'InstanceId': instanceId,
+				'InstanceName': instanceName,
+				'Count': findingCount,
+				'Severity': severity
+			}
+			writer.writerow(row)
+			counter+=1
